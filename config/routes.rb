5a26f8c8984a0b31cb2a1029a9a74cab4beebf39
Jodything <Jodything@gmail.com>
2015-01-07 10:11:08 -0500

    bower and polymer implementation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'houses/index'
-
+  root 'houses#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
